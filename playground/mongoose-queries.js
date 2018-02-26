59968ac313450a38594059889fc808e5b300c70a
trekker473 <trek.porter@gmail.com>
2018-02-25 23:04:51 -0800

    mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var userId =  '5a91f525ee48f6ca3d7206f0';
+// var id = '5a93a818ff1014d13a92ad10';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById(userId).then((user) => {
+  if(!user) {
+    return console.log('Id not found');
+  }
+  console.log('User by id', user);
+}).catch((e) => console.log(e));
