1c7eb7d2fe738366b1fdc38259a3d655d97b2640
trekker473 <trek.porter@gmail.com>
2018-03-18 15:39:58 -0700

    added DELETE/user/me/token

--- server/server.js
+++ server/server.js
@@ -118,7 +118,7 @@ app.get('/users/me', authenticate, (req, res) =>{
   res.send(req.user);
 });
 
-//POST /users/login 
+//POST /users/login
 app.post('/users/login', (req, res) => {
  var body = _.pick(req.body, ['email', 'password']);
 
@@ -131,6 +131,15 @@ app.post('/users/login', (req, res) => {
 
  });
 });
+
+app.delete('/users/me/token', authenticate, (req,res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 
 
 app.listen(port, () => {
